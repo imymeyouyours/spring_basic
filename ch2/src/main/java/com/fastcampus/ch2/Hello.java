685da2689c16dab99e1d01c4094559742373f092
InHyeKyeong <kih9079@naver.com>
2026-04-18 16:26:53 +0900

    ch2 - hello

---
+++ ch2/src/main/java/com/fastcampus/ch2/Hello.java
@@ -0,0 +1,27 @@
+package com.fastcampus.ch2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 1. 원격 호출가능한 프로그램으로 등록 
+@Controller
+public class Hello {
+
+	int iv = 10; // 인스턴스 변수
+	static int cv = 20; // static 변수
+
+	// 2. URL과 메서드를 연결
+	@RequestMapping("/hello")
+	private void main() { // 인스턴스 메서드 - iv, cv를 둘다 사용 가
+		System.out.println("hello - static");
+		System.out.println(cv); // ok
+//		System.out.println(iv); // ok
+	}
+
+	public static void main2() { // static 메서드 - cv만 사용 가능
+		System.out.println(cv); // ok
+//		System.out.println(iv);		// 에러
+
+	}
+
+}
---
+++ ch2/src/main/java/com/fastcampus/ch2/Main.java
@@ -0,0 +1,12 @@
+package com.fastcampus.ch2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+	}
+
+}
