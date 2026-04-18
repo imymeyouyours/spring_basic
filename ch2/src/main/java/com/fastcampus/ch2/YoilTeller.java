abbf6afe8cf54d41d18e59e5b0e5457235d6e57f
InHyeKyeong <kih9079@naver.com>
2026-04-18 17:26:06 +0900

    5) YoilTeller 출력

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -1,30 +1,46 @@
 package com.fastcampus.ch2;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Calendar;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// 년월일을 입력하면 요일을 알려주는 프로그램 
+@RestController
 public class YoilTeller {
-	
-	public static void main(String[] args) {
+
+//	public static void main(String[] args) {
+	@RequestMapping("getYoil")
+	public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		// 1.입력
-		String year = args[0];
-		String month = args[1];
-		String day = args[2];
-		
+		String year = request.getParameter("year");
+		String month = request.getParameter("month");
+		String day = request.getParameter("day");
+
 		int yyyy = Integer.parseInt(year);
 		int mm = Integer.parseInt(month);
 		int dd = Integer.parseInt(day);
-		
+
 		// 2.작업
 		Calendar cal = Calendar.getInstance();
-		cal.set(yyyy, mm-1, dd);
-		
+		cal.set(yyyy, mm - 1, dd);
+
 		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
 		char yoil = " 일월화수목금토".charAt(dayOfWeek);
-		
-		//3.출력
-		System.out.println(year+"년"+month+"월"+day+"일은 ");
-		System.out.println(yoil + "요일입니다.");
-		
+
+		// 3.출력
+
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter(); // reponse객체에서 브라우저로의 출력 스트림을 얻는다.
+		out.println(year + "년 " + month + "월 " + day + "일은 ");
+		out.println(yoil + "요일입니다.");
+
 	}
 
 }
