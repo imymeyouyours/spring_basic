790cdef5af0dc02cdc2f614015af712e63b840b5
InHyeKyeong <kih9079@naver.com>
2026-04-22 22:02:41 +0900

    21) @ResponseStatus, isErrorPage=”true”

--- ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -2,19 +2,22 @@ package com.fastcampus.ch2;
 
 import java.io.FileNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ExceptionController {
 
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 200 -> 500
 	@ExceptionHandler(Exception.class)
 	public String chatcher(Exception ex, Model m) {
 		System.out.println("m = " + m);
 		System.out.println("catcher() in ExceptionController");
-		m.addAttribute("ex", ex);
+//		m.addAttribute("ex", ex);
 		return "error";
 	}
 
