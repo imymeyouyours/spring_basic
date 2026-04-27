61c63d44e9a326dd869d46480e28bb452c9c395b
InHyeKyeong <kih9079@naver.com>
2026-04-27 18:27:10 +0900

    34) DB TDD

--- ch3/src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
+++ ch3/src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -8,6 +8,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.Date;
+import java.sql.PreparedStatement;
 
 import static org.junit.Assert.*;
 
@@ -17,6 +20,119 @@ public class DBConnectionTest2Test {
 
     @Autowired
     DataSource ds;
+
+    @Test
+    public void inserUserTest() throws Exception {
+        User user = new User("asdf", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+
+        deleteAll();
+
+        int rowCnt = inserUser(user);
+
+        assertTrue(rowCnt == 1);
+    }
+
+    @Test
+    public void selectUserTest() throws Exception {
+
+        deleteAll();
+        User user = new User("asdf", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        inserUser(user);
+
+        User user2 = selectUser("asdf");
+        assertTrue(user2.getId().equals("asdf"));
+
+    }
+
+    @Test
+    public void deleteUserTest() throws Exception {
+
+        deleteAll();
+        int rowCnt = deleteUser("asdf");
+
+        assertTrue(rowCnt == 0);
+
+        User user = new User("asdf", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        rowCnt = inserUser(user);
+        assertTrue(rowCnt == 1);
+
+        rowCnt = deleteUser("asdf");
+        assertTrue(rowCnt == 1);
+
+        assertTrue(selectUser(user.getId())==null);
+
+    }
+
+    public int deleteUser(String id) throws Exception {
+
+        Connection conn = ds.getConnection();
+
+        String sql = "delete from user_info where id = ?";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);   // SQL Injection 공격, 성능향상
+        pstmt.setString(1, id);
+
+        return pstmt.executeUpdate();     // insert, delete, update
+    }
+
+    public User selectUser(String id) throws Exception {
+
+        Connection conn = ds.getConnection();
+
+        String sql = "select * from user_info where id = ?";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);   // SQL Injection 공격, 성능향상
+        pstmt.setString(1, id);
+        ResultSet rs = pstmt.executeQuery();     // select
+
+        if (rs.next()) {
+            User user = new User();
+            user.setId(rs.getString(1));
+            user.setPwd(rs.getString(2));
+            user.setName(rs.getString(3));
+            user.setEmail(rs.getString(4));
+            user.setBirth(new Date(rs.getDate(5).getTime()));
+            user.setSns(rs.getString(6));
+            user.setReg_date(new Date(rs.getTimestamp(7).getTime()));
+
+            return user;
+        }
+
+        return null;
+    }
+
+    private void deleteAll() throws Exception {
+
+        Connection conn = ds.getConnection();
+
+        String sql = "delete from user_info";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);   // SQL Injection 공격, 성능향상
+        pstmt.executeUpdate();     // insert, delete, update
+    }
+
+    // 사용자 정보를 user_info 테이블에 저장하는 메서드
+    public int inserUser(User user) throws Exception {
+
+        Connection conn = ds.getConnection();
+
+//        insert into user_info (id, pwd, name, email, birth, sns, reg_date)
+//        values ('asdf2','1234','bbb@bbb.com','2021-10-01','facebook',now());
+
+        String sql = "insert into user_info values (?,?,?,?,?,?,now())";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);   // SQL Injection 공격, 성능향상
+        pstmt.setString(1, user.getId());
+        pstmt.setString(2, user.getPwd());
+        pstmt.setString(3, user.getName());
+        pstmt.setString(4, user.getEmail());
+        pstmt.setDate(5, new java.sql.Date(user.getBirth().getTime()));
+        pstmt.setString(6, user.getSns());
+
+        int rowCnt = pstmt.executeUpdate();     // insert, delete, update
+
+        return rowCnt;
+    }
 
     @Test
     public void springJdbcConnectionTest() throws Exception {
